876cd735dc6fbde7fffdd0dafd04b463e9e6a0e4
League <league@WTS8.local>
2018-02-16 18:29:08 -0800

    PrimeOrNot (ip)

---
+++ src/PrimeOrNot.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+public class PrimeOrNot {
+	public static void main(String[] args) {
+		
+	String anynumber = JOptionPane.showInputDialog("Give me a number.");
+	int number = Integer.parseInt(anynumber);
+	for(int i=0;i<3;i++) {
+	for(int r=0;r<2;r++)	{
+		
+	}
+		}
+	}
+}
