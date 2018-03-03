3b078463a5868de3bfccd91e95a76e2a34b5074c
Fangtastic7 <league@WorthyMini-5.attlocal.net>
2018-03-02 18:33:54 -0800

    Circle Ring, Fibonacci, GoofyNames, Guessing Pi, For loop Gauntlet (ip)

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,10 +1,16 @@
 
 public class Fibonacci {
 public static void main(String[] args) {
-	System.out.println("0 1 1 ");
-	for(int i=3;i<12;i++) {
-		System.out.println((i-1)+(i-2));
-		
+	int first = 0;
+	int second = 1;
+	int third = first + second;
+	System.out.println(first);
+	System.out.println(second);
+	for(int i=2;i<13;i++) {
+		System.out.println(third);
+		first=second; 
+		second=third;
+		third=first+second;
 	}
 }
 }
